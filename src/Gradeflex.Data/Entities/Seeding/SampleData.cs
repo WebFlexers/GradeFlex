f76319917e191717502109f94283bd254748d1f8
StyleM <57811193+mStylias@users.noreply.github.com>
2023-01-13 20:58:47 +0200

    Add seeding class (currently empty)

---
+++ src/Gradeflex.Data/Entities/Seeding/SampleData.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Gradeflex.Data.Entities.Seeding;
+
+public static class SampleData
+{
+    public static void Seed(ModelBuilder modelBuilder)
+    {
+
+    }
+}
