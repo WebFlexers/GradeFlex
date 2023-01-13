f5848cdc26a752cc80049276181c31d7c359dc6c
StyleM <57811193+mStylias@users.noreply.github.com>
2023-01-13 20:58:02 +0200

    Add user entity and config

---
+++ src/Gradeflex.Data/Entities/Configurations/UserConfig.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Gradeflex.Data.Entities.Configurations;
+
+public class UserConfig : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.HasKey(user => user.Username);
+
+        builder.Property(user => user.Username)
+            .IsRequired(true)
+            .HasMaxLength(45);
+
+        builder.Property(user => user.Password)
+            .IsRequired(true)
+            .HasMaxLength(100);
+
+        builder.Property(user => user.Role)
+            .IsRequired(true)
+            .HasMaxLength(45);
+    }
+}
---
+++ src/Gradeflex.Data/Entities/User.cs
@@ -0,0 +1,8 @@
+﻿namespace Gradeflex.Data.Entities;
+
+public class User
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string Role { get; set; }
+}
